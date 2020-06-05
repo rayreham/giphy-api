816703c2da9f74b5eaba7104669e547f218a8ec9
rayreham <rayreham@yahoo.com>
2020-06-05 13:06:25 -0400

    Connecting to API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { Component } from "react";
-import './App.css';
-import Gif from "./components/Gif";
+import React, { Component } from 'react';
+import axios from 'axios';
 
+export default class App extends Component {
 
-class App extends Component {
-  
+  state = {
+    gifs: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    this.performSearch();
+    
+  }
+
+  performSearch = (word = '') => {
+    const API_KEY = process.env.REACT_APP_GIF_API_KEY;
+    const url =  ` http://api.giphy.com/v1/gifs/search?q=${word}&api_key=`;
+    axios.get(url, { params: { key: API_KEY } })
+      .then(response => {
+        this.setState({
+          gifs: response.data.data,
+          loading: false
+        });
+      })
+      
+      .catch((err) => console.log(err));
+  }
+
   render() {
     return (
-      <div className="App">
-        <Gif word = "hello" /> 
+      <div>
+        
       </div>
     );
   }
 }
-
-export default App;
