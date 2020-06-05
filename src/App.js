ed3a72944e24e56c47325d28fc10798c16b0404a
rayreham <rayreham@yahoo.com>
2020-06-05 14:36:09 -0400

    Component change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import SearchFieldComponent from './components/SearchFieldComponent';
+import AppComponent from './components/AppComponent';
 
 export default class App extends Component {
-
+//gif states as per api
   state = {
     gifs: [],
     loading: true
@@ -13,6 +15,7 @@ export default class App extends Component {
     
   }
 
+  //take in word and queries it in api
   performSearch = (word = '') => {
     const API_KEY = process.env.REACT_APP_GIF_API_KEY;
     const url =  ` http://api.giphy.com/v1/gifs/search?q=${word}&api_key=`;
@@ -30,7 +33,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <SearchFieldComponent onSearch={this.performSearch} loading={this.state.loading}/>
+        <AppComponent data={this.state.gifs} />
       </div>
     );
   }
