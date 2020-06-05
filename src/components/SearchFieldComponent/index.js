42f8d22c5f6817c25accb13aebcebc4f1151167b
rayreham <rayreham@yahoo.com>
2020-06-05 14:33:54 -0400

    Header change

--- src/components/SearchFieldComponent/index.js
+++ src/components/SearchFieldComponent/index.js
@@ -5,7 +5,7 @@ export default function SearchFieldComponent(props) {
   return (
     <header className="main-header">
       <div className="inner">
-        <h1 className="main-title">Gif Api Search</h1>
+        <h1 className="main-title">Giphy search</h1>
         <SearchForm onSearch={props.onSearch} />
       </div>
     </header>
